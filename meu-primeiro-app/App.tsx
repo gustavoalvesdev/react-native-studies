0f02b4efecf178bd5f8a5b792c4dd23d6ba9f2b0
= <=>
2023-10-20 20:05:51 -0300

    Entendendo Componentes

--- meu-primeiro-app/App.tsx
+++ meu-primeiro-app/App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Gustavo Alves da Silva! Olá Mundo!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Text>Meu Primeiro App</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

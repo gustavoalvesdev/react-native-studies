ffefe108ad996acaba943845be718dce62563887
= <=>
2023-10-20 20:28:06 -0300

    Componente SafeAreaView (só funciona no iOS)

--- meu-primeiro-app/App.tsx
+++ meu-primeiro-app/App.tsx
@@ -1,16 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { PizzaItem } from './components/PizzaItem';
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>Meu Primeiro App</Text>
-        <PizzaItem />
         <PizzaItem />
         <PizzaItem />
         <PizzaItem />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
 
 export default App;
